f2b6f771a05f26cf88e03a0a6d1035767e82c5c5
gmnawab <gmnawab25@gmail.com>
2024-02-17 20:34:17 +0600

    created game start functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,36 @@
+// function play() {
+//     const homeSection = document.querySelector(".home");
+//     const playGroundSection = document.querySelector(".play-ground");
+//     homeSection.classList.add("hidden");
+//     playGroundSection.classList.remove("hidden");
+// }
+
+//short way by function
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add("hidden");
+}
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove("hidden");
+}
+function setBgColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-amber-400');
+    element.classList.add('text-white');
+}
+function randomAlphabet() {
+    const showText = document.getElementById('show-text');
+    let str = 'abcdefghijklmnopqrstuvwxyz';
+    let string = str.split('');
+    let randomNumber = Math.round(Math.random() * 25);
+    let alphabet = string[randomNumber];
+    showText.innerText = alphabet.toUpperCase();
+    setBgColorById(alphabet);
+}
+
 function play() {
-    const homeSection = document.querySelector(".home");
-    const playGroundSection = document.querySelector(".play-ground");
-    homeSection.classList.add("hidden");
-    playGroundSection.classList.remove("hidden");
+    hideElementById('home');
+    showElementById('play-ground');
+    randomAlphabet();
 }
